43881746cc77a93aef9dbb0350f19b6b75ea1632
asn007 <andrey.sin98@gmail.com>
2015-06-11 11:49:14 +0300

    I always forget about license

--- src/main/java/ru/nloader/patching/Patch.java
+++ src/main/java/ru/nloader/patching/Patch.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2015 asn007 aka Andrey Sinitsyn <andrey.sin98@gmail.com>
+ *
+ *  This file (Patch.java) is part of nPatch.
+ *
+ *      ${PROJECT_NAME} is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      ${PROJECT_NAME} is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with ${PROJECT_NAME}.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ru.nloader.patching;
 
 import java.io.File;
@@ -9,7 +28,7 @@ import java.util.Date;
 public class Patch {
     private File patchFile;
     private Date releaseDate;
-
+    private PatchNote patchNote;
     public Patch(File patchFile) {
         this.patchFile = patchFile;
     }
--- src/main/java/ru/nloader/patching/PatchNote.java
+++ src/main/java/ru/nloader/patching/PatchNote.java
@@ -1,7 +1,49 @@
+/*
+ * Copyright (C) 2015 asn007 aka Andrey Sinitsyn <andrey.sin98@gmail.com>
+ *
+ *  This file (PatchNote.java) is part of nPatch.
+ *
+ *      nPatch is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      nPatch is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with nPatch.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ru.nloader.patching;
 
 /**
  * Created by asn007 on 11.06.2015.
  */
 public class PatchNote {
+    private String patchTitle;
+    private String patchChanges;
+
+    public PatchNote(String patchTitle, String patchChanges) {
+        this.patchTitle = patchTitle;
+        this.patchChanges = patchChanges;
+    }
+
+    public String getPatchTitle() {
+        return patchTitle;
+    }
+
+    public void setPatchTitle(String patchTitle) {
+        this.patchTitle = patchTitle;
+    }
+
+    public String getPatchChanges() {
+        return patchChanges;
+    }
+
+    public void setPatchChanges(String patchChanges) {
+        this.patchChanges = patchChanges;
+    }
 }
